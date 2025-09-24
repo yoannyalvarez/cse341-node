e94f7f83f4aa5d8b41648edf7ffa95e6a9127238
yoannyalvarez <imasmilergirl@gmail.com>
2025-09-23 23:31:34 -0400

    fixing erros

--- library/routes/swagger.js
+++ library/routes/swagger.js
@@ -2,19 +2,7 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('../swagger-output.json');
 
-router.use('/api-docs', () => {
-    try {
-        swaggerUi.serve // Middleware to serve Swagger UI assets
-    } catch (error) {
-        console.error('Failed to load Swagger UI', error);
-    }
-});
-router.get('/api-docs', () => {
-    try {
-        swaggerUi.setup(swaggerDoc) // Middleware to set up Swagger UI with the generated documentation
-    } catch (error) {
-        console.error('Failed to set up Swagger UI', error);
-    }
-});
+router.use('/api-docs', swaggerUi.serve)
+router.get('/api-docs', swaggerUi.setup(swaggerDoc))
 
 module.exports = router;

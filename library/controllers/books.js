e66605975735cbde7ce9b8db9b25e199361490cd
yoannyalvarez <imasmilergirl@gmail.com>
2025-09-27 15:26:22 -0400

    adding try/catch blocks

--- library/controllers/books.js
+++ library/controllers/books.js
@@ -3,31 +3,32 @@ const { ObjectId } = require('mongodb');
 
 const getAll = async (req, res) => {
     //#swagger.tags = ['Books']
-    const database = await mongodb.getDatabase();
-    const result = await database.collection('books').find();
-    result.toArray(err => {
-        if (err) {
+    try {
+        const database = await mongodb.getDatabase();
+        const result = await database.collection('books').find();
+        result.toArray().then((books) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(books);
+        });
+    } catch (err) {
             res.status(500).json(result.error || 'Some error occurred while retrieving the books.');
-        }
-    }).then((books) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(books);
-    });
+    }
+    
 };   
 
 const getById = async (req, res) => {
     //#swagger.tags = ['Books']
-    const bookId = new ObjectId(req.params.id);
-    const database = await mongodb.getDatabase();
-    const result = await database.collection('books').find({_id: bookId});
-    result.toArray(err => {
-        if (err) {
-            res.status(500).json(result.error || 'Some error occurred while retrieving the book.');
-        }
-    }).then((books) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(books[0]);
-    });  
+    try {
+        const bookId = new ObjectId(req.params.id);
+        const database = await mongodb.getDatabase();
+        const result = await database.collection('books').find({_id: bookId});
+        result.toArray().then((books) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(books[0]);
+        });
+    } catch (err) {
+        res.status(500).json(result.error || 'Some error occurred while retrieving the book.');
+    }  
 }; 
 
 const createBook = async (req, res) => {
@@ -40,13 +41,18 @@ const createBook = async (req, res) => {
         publication_date: req.body.publication_date,
         category: req.body.category
     };
-    const database = await mongodb.getDatabase();
-    const result = await database.collection('books').insertOne(newBook);
-    if (result.acknowledged !== 1) {
-        res.status(201).json(result.insertedId);
-    } else {
+    try {
+        const database = await mongodb.getDatabase();
+        const result = await database.collection('books').insertOne(newBook);
+        if (result.acknowledged !== 1) {
+            res.status(201).json(result.insertedId);
+        } 
+    } catch (err) {
         res.status(500).json(result.error || 'Some error occurred while creating the book.');
     }
+
+
+ 
 };
 
 const updateBook = async (req, res) => {
@@ -60,24 +66,32 @@ const updateBook = async (req, res) => {
         publication_date: req.body.publication_date,
         category: req.body.category
     };
-    const database = await mongodb.getDatabase();
-    const result = await database.collection('books').updateOne({_id: bookId}, {$set: updatedBook});
-    if (result.modifiedCount !== 1) {
+    try {
+        const database = await mongodb.getDatabase();
+        const result = await database.collection('books').updateOne({_id: bookId}, {$set: updatedBook});
+        if (result.modifiedCount !== 1) {
+            throw err;
+        } else {
+            res.status(204).end();
+        }
+    } catch (err) {
         res.status(500).json(result.error || 'Some error occurred while updating the book.');
-    } else {
-        res.status(204).end();
     }
 };
     
 const removeBook = async (req, res) => {
     //#swagger.tags = ['Books']
-    const bookId = new ObjectId(req.params.id);
-    const database = await mongodb.getDatabase();
-    const result = await database.collection('books').deleteOne({_id: bookId});
-    if (result.deletedCount !== 1) {
+    try {
+        const bookId = new ObjectId(req.params.id);
+        const database = await mongodb.getDatabase();
+        const result = await database.collection('books').deleteOne({_id: bookId});
+        if (result.deletedCount !== 1) {
+            throw err;
+        } else {
+            res.status(204).end();
+        }
+    } catch (err) {
         res.status(500).json(result.error || 'Some error occurred while deleting the book.'); 
-    } else {
-        res.status(204).end();
     }
 };
 
